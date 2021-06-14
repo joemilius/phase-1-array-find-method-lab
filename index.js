0c9ff0c4712bb2def07ea17d3bc26441b7d79174
JoeMilius <joemilius@gmail.com>
2021-06-14 14:30:12 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function superbowlWin(record){  
+    let chiefsWins = record.find(superbowlObject => superbowlObject.result === 'W')
+    console.log(chiefsWins)
+    if (chiefsWins){
+        return chiefsWins.year
+    }
+}    
 
+
+// function superbowlWin(array) {
+//     return (array.find(array => array.result === "W"));
+// //     if (win){
+// //         return Object.values(win);
+// //     }     
+//    } 
